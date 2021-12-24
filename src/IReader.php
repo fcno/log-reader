60fe87820cc61765ba134c73b26c66bef5cdd490
fcno <fabiocassiano@jfes.jus.br>
2021-12-24 16:10:45 -0300

    Criar Interface para a leitura de log

---
+++ src/IReader.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Fcno\LogReader;
+
+use Illuminate\Support\Collection;
+
+/**
+ * @author Fábio Cassiano <fabiocassiano@jfes.jus.br>
+ */
+interface IReader
+{
+    /**
+     * Define o file system de armazenamento dos logs.
+     *
+     * @param string  $disk nome do file system
+     */
+    public function from(string $disk): static;
+
+    /**
+     * Informações sobre um determinado log.
+     *
+     * @param string  $log_file
+     *
+     * @return static
+     *
+     * @throws \Fcno\LogReader\Exceptions\FileNotFoundException
+     */
+    public function infoAbout(string $log_file): static;
+
+    /**
+     * Resultados integrais e sem paginação.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public function get(): Collection;
+
+    /**
+     * Resultados paginados.
+     *
+     * Coleção vazia ou com a quantidade de itens menor que a solicitada se o
+     * arquivo já tiver chegado ao final do arquivo.
+     *
+     * @param int  $page
+     * @param int  $per_page
+     *
+     * @return \Illuminate\Support\Collection
+     *
+     * @throws \RuntimeException
+     */
+    public function paginate(int $page, int $per_page): Collection;
+}

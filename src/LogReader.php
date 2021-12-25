55a7662766c2b3c4d5fd5c2eb82a2f8af4de340c
fcno <fabiocassiano@jfes.jus.br>
2021-12-25 02:54:28 -0300

    Implementar o log reader para ler o File System

--- src/LogReader.php
+++ src/LogReader.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Storage;
 
 /**
+ * Manipular os arquivos de log do file system.
+ *
  * @author Fábio Cassiano <fabiocassiano@jfes.jus.br>
  */
 final class LogReader
@@ -29,20 +31,6 @@ final class LogReader
     private $log_file;
 
     /**
-     * @var int
-     *
-     * Página da paginação que será exibida quando o resultado for paginado.
-     */
-    private $page;
-
-    /**
-     * @var int
-     *
-     * Quantidada de registros por página que serão exibidos quando da paginação.
-     */
-    private $per_page;
-
-    /**
      * Define o file system de armazenamento dos logs da aplicação de acordo
      * com o nome informado.
      *
@@ -56,89 +44,24 @@ final class LogReader
     }
 
     /**
-     * Informações completas de todos os registros de um determinado log.
-     *
-     * Informações contidas em cada registro:
-     * - date    - data do evento
-     * - time    - hora do evento
-     * - env     - ambiente em que o evento ocorreu
-     * - level   - nível do evento nos termos da PSR-3
-     * - message - mensagem
-     * - context - mensagem de contexto
-     * - extra   - dados extras sobre o evento
-     *
-     * @param string  $log_file Ex.: laravel-2000-12-30.log
-     *
-     * @return static
-     *
-     * @throws \RuntimeException
-     */
-    public function fullSummary(int $page, int $per_page): Collection
-    {
-        throw_if($page < 1 || $per_page < 1);
-
-        $files = collect($this->file_system->files())
-                    ->sortDesc()
-                    ->slice(
-                        offset: ($page - 1) * $per_page,
-                        length: $per_page
-                    );
-
-        $summary = collect();
-
-        foreach ($files as $file) {
-            $this->log_file = $file;
-
-            $summary->push(
-                $this->getDailySummary($file)
-            );
-        }
-
-        return $summary;
-    }
-
-    /**
-     * Informações completas de todos os registros de um determinado log.
-     *
-     * Informações contidas em cada registro:
-     * - date    - data do evento
-     * - time    - hora do evento
-     * - env     - ambiente em que o evento ocorreu
-     * - level   - nível do evento nos termos da PSR-3
-     * - message - mensagem
-     * - context - mensagem de contexto
-     * - extra   - dados extras sobre o evento
-     *
-     * @param string  $log_file Ex.: laravel-2000-12-30.log
-     *
-     * @return static
-     *
-     * @throws \Fcno\LogReader\Exceptions\FileNotFoundException
-     */
-    public function fullInfoAbout(string $log_file): static
-    {
-        throw_if($this->file_system->missing($log_file), FileNotFoundException::class);
-
-        $this->log_file = $log_file;
-
-        return $this;
-    }
-
-    /**
-     * Registros do arquivo de log.
+     * Todos os arquivos de log do file system ordenados do mais recente para o
+     * mais antigo.
      *
      * @return \Illuminate\Support\Collection
      */
     public function get(): Collection
     {
-        return $this->readLog();
+        return collect($this->file_system->files())
+                ->sortDesc()
+                ->values();
     }
 
     /**
-     * Registros do arquivo de log de maneira paginada.
+     * Arquivos de log do file system ordenados do mais recente para o mais
+     * antigo paginados.
      *
      * Retornará uma coleção vazia ou com a quantidade de itens menor que a
-     * solicitada se o arquivo já tiver chegado ao final do arquivo.
+     * solicitada se o file system não possuir mais arquivos para leitura.
      *
      * @param int  $page
      * @param int  $per_page
@@ -151,125 +74,10 @@ final class LogReader
     {
         throw_if($page < 1 || $per_page < 1);
 
-        $this->page     = $page;
-        $this->per_page = $per_page;
-
-        return $this->readLog();
-    }
-
-    /**
-     * Sumário de determinado log.
-     *
-     * Sumariza:
-     * - Data do log
-     * - Quantidade de registros por level
-     *
-     * @param string  $log_file Ex.: laravel-2000-12-30.log
-     *
-     * @return \Illuminate\Support\Collection
-     *
-     * @throws \Fcno\LogReader\Exceptions\FileNotFoundException
-     */
-    public function getDailySummary(string $log_file): Collection
-    {
-        throw_if($this->file_system->missing($log_file), FileNotFoundException::class);
-
-        $this->log_file = $log_file;
-
-        $summary = $this->readLog();
-
-        return $this->readyToGoSummary($summary);
-    }
-
-    /**
-     * Prepara o sumário para ser retornado ao chamador
-     *
-     * Sumariza:
-     * - Data do log
-     * - Quantidade de registros por level
-     *
-     * @param \Illuminate\Support\Collection  $summary_in_process
-     *
-     * @return \Illuminate\Support\Collection
-     */
-    private function readyToGoSummary(Collection $summary_in_process): Collection
-    {
-        $filtered = $summary_in_process->countBy('level');
-
-        $filtered->put('date', $summary_in_process->first()->get('date'));
-
-        return $filtered;
-    }
-
-    /**
-     * Lê o arquivo de log informado e o retorna como coleção.
-     *
-     * O método leva em consideração se foi informada a necessidade de o
-     * resultado ser paginado.
-     *
-     * @return \Illuminate\Support\Collection
-     */
-    private function readLog(): Collection
-    {
-        $data = collect();
-        $line_generator = $this->getLineGenerator();
-
-        // Lê linha a linha o log. Boa prática não carregar tudo em memória.
-        foreach ($line_generator as $record) {
-            preg_match(
-                Regex::PATTERN,
-                (string) $record,
-                $output_array
-            );
-
-            $data->push(
-                $this->filteredData($output_array)
-            );
-        }
-
-        return $data;
-    }
-
-    /**
-     * Retorna um line generator de acordo com a necessidade ou não de
-     * paginação do resultado
-     *
-     * @return \LimitIterator|\Generator
-     */
-    private function getLineGenerator(): \LimitIterator|\Generator
-    {
-        $line_generator = LineReader::readLines($this->getFullPath());
-
-        return ($this->page && $this->per_page)
-        ? new \LimitIterator(
-            iterator: $line_generator,
-            offset: ($this->page - 1) * $this->per_page,
-            limit: $this->per_page
-        )
-        : $line_generator;
-    }
-
-    /**
-     * Filtra o array informado permanecendo apenas os índices que integram o
-     * registro.
-     *
-     * @param array  $data
-     *
-     * @return \Illuminate\Support\Collection
-     */
-    private function filteredData(array $data): Collection
-    {
-        return collect($data)
-                ->only(['date', 'time', 'env', 'level', 'message', 'context', 'extra']);
-    }
-
-    /**
-     * Caminho completo do arquivo de log que está sendo trabalhado
-     *
-     * @return string  Full path
-     */
-    private function getFullPath(): string
-    {
-        return $this->file_system->path($this->log_file);
+        return $this->get()
+                    ->slice(
+                        offset: ($page - 1) * $per_page,
+                        length: $per_page
+                    );
     }
 }
--- tests/LogReaderTest.php
+++ tests/LogReaderTest.php
@@ -6,12 +6,11 @@
  * @link https://pestphp.com/docs/
  */
 
-use Fcno\LogReader\Exceptions\FileNotFoundException;
 use Fcno\LogReader\Facades\LogReader;
 use Fcno\LogReader\LogReader as Reader;
 use Fcno\LogReader\Tests\Stubs\LogGenerator;
 use Illuminate\Support\Facades\Storage;
-use illuminate\support\Str;
+use Illuminate\Support\Str;
 
 beforeEach(function () {
     $this->fs_name = 'log-aplicacao';
@@ -22,117 +21,60 @@ beforeEach(function () {
 
     $this->file_name = Str::of('laravel-')
                             ->append(now()->format('Y-m-d'))
-                            ->finish('.log');
+                            ->finish('.log')
+                            ->__toString();
 });
 
 test('o facade retorna o objeto da classe corretamente', function () {
     expect(LogReader::from($this->fs_name))->toBeInstanceOf(Reader::class);
 });
 
-test('lança exceção ao tentar ler sumário de arquivo inexistente', function () {
-    expect(
-        fn () => LogReader::from($this->fs_name)
-                            ->getDailySummary('laravel-2500-12-30.log')
-    )->toThrow(FileNotFoundException::class);
-});
+test('obtém todos os arquivos de log do file system ordenados do mais recente para o mais antigo', function () {
+    $amount = 10;
+    $last_log_file = Str::of('laravel-')
+                        ->append(
+                            now()
+                            ->subDays($amount - 1)
+                            ->format('Y-m-d')
+                        )
+                        ->finish('.log')
+                        ->__toString();
 
-test('sumariza corretamente a quantidade de logs de um determinado tipo e a sua data', function () {
-    $level           = 'alert';
-    $amount          = 5;
-    $appended_level  = 'debug';
-    $appended_amount = 10;
-
-    LogGenerator::on($this->fs_name)
-                ->create(['level' => $level])
-                ->count(files: 1, records: $amount)
-                ->appendLevel(
-                    log_file: $this->file_name,
-                    records: $appended_amount,
-                    level: $appended_level
-                );
-
-    $response = LogReader::from($this->fs_name)
-                            ->getDailySummary($this->file_name);
-
-    expect($response)
-    ->get('date')->toBe(now()->format('Y-m-d'))
-    ->get($level)->toBe($amount)
-    ->get($appended_level)->toBe($appended_amount)
-    ->get('emergency')->toBeNull();
-});
-
-test('obtém todas as informações sobre os registros de um determinado arquivo de log', function () {
     LogGenerator::on($this->fs_name)
                 ->create(null)
-                ->count(files: 1, records: 1);
+                ->count(files: $amount, records: 1);
 
-    $response = LogReader::from($this->fs_name)
-                            ->fullInfoAbout($this->file_name)
-                            ->get();
+    $response = LogReader::from($this->fs_name)->get();
 
-    expect($response->first())
-    ->toHaveKeys(['date', 'time', 'env', 'level', 'message', 'context', 'extra'])
-    ->get('date')->toBe(now()->format('Y-m-d'));
+    expect($response)
+    ->toHaveCount($amount)
+    ->first()->toBe($this->file_name)
+    ->last()->toBe($last_log_file);
 });
 
 test('lança exceção ao tentar paginar com página ou por página menor que 1', function () {
-
-    LogGenerator::on($this->fs_name)
-                ->create(null)
-                ->count(files: 1, records: 1);
-
     expect(
         fn () => LogReader::from($this->fs_name)
-                            ->fullInfoAbout($this->file_name)
-                            ->paginate(page:-1, per_page: 1)
+                                ->paginate(page:-1, per_page: 1)
     )->toThrow(RuntimeException::class);
 
     expect(
         fn () => LogReader::from($this->fs_name)
-                            ->fullInfoAbout($this->file_name)
-                            ->paginate(page: 1, per_page: -1)
+                                ->paginate(page: 1, per_page: -1)
     )->toThrow(RuntimeException::class);
 });
 
-test('retorna a quantidade de registros do arquivo de log de acordo com a paginação solicitada', function ($page, $expect) {
-    LogGenerator::on($this->fs_name)
-                ->create(null)
-                ->count(files: 1, records: 17);
-
-    $response = LogReader::from($this->fs_name)
-                            ->fullInfoAbout($this->file_name)
-                            ->paginate(page: $page, per_page: 5);
-
-    expect($response)->toHaveCount($expect);
-})->with([
-    [3, 5], // página 3 retorna 5 registros. Página completa
-    [4, 2], // página 4 retorna 2 registros. Página incompleta, chegou-se ao fim
-    [5, 0]  // página 5 retorna 0 registros. Paginação já chegou ao fim
-]);
-
-test('lança exceção ao tentar paginar os arquivos com página ou por página menor que 1', function () {
-    expect(
-        fn () => LogReader::from($this->fs_name)
-                            ->fullSummary(page:-1, per_page: 1)
-    )->toThrow(\RuntimeException::class);
-
-    expect(
-        fn () => LogReader::from($this->fs_name)
-                            ->fullSummary(page: 1, per_page: -1)
-    )->toThrow(\RuntimeException::class);
-});
-
-test('retorna a quantidade de arquivos esperada de acordo com a paginação solicitada', function ($page, $expect) {
+test('obtém a quantidade de arquivos esperada de acordo com a paginação solicitada ordenados do mais recente para o mais antigo', function ($page, $expect) {
     LogGenerator::on($this->fs_name)
                 ->create(null)
                 ->count(files: 17, records: 1);
 
     $response = LogReader::from($this->fs_name)
-                            ->fullSummary(page: $page, per_page: 5);
+                            ->paginate(page: $page, per_page: 5);
 
     expect($response)->toHaveCount($expect);
 })->with([
-    [3, 5], // página 3 retorna 5 arquivos. Página completa
+    [2, 5], // página 3 retorna 5 arquivos. Página completa
     [4, 2], // página 4 retorna 2 arquivos. Página incompleta, chegou-se ao fim
     [5, 0]  // página 5 retorna 0 arquivos. Paginação já chegou ao fim
 ]);

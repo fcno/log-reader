fdbafaec07cb49c55c31b7058ef37f319cc21e7c
fcno <fabiocassiano@jfes.jus.br>
2021-12-28 15:04:44 -0300

    Alterar rule set do PHPFIXER para Symfony

--- .php_cs.dist.php
+++ .php_cs.dist.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * @link https://github.com/FriendsOfPHP/PHP-CS-Fixer
+ */
+
 $finder = Symfony\Component\Finder\Finder::create()
     ->in([
         __DIR__ . '/src',
@@ -12,29 +16,16 @@ $finder = Symfony\Component\Finder\Finder::create()
 
 return (new PhpCsFixer\Config())
     ->setRules([
-        '@PSR12' => true,
-        'array_syntax' => ['syntax' => 'short'],
-        'ordered_imports' => ['sort_algorithm' => 'alpha'],
-        'no_unused_imports' => true,
+        '@Symfony' => true,
         'not_operator_with_successor_space' => true,
-        'trailing_comma_in_multiline' => true,
-        'phpdoc_scalar' => true,
-        'unary_operator_spaces' => true,
-        'binary_operator_spaces' => true,
         'blank_line_before_statement' => [
-            'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
+            'statements' => ['break', 'case', 'continue', 'declare', 'default', 'exit', 'goto', 'include', 'include_once', 'phpdoc', 'require', 'require_once', 'return', 'switch', 'throw', 'try'],
         ],
-        'phpdoc_single_line_var_spacing' => true,
-        'phpdoc_var_without_name' => true,
-        'class_attributes_separation' => [
-            'elements' => [
-                'method' => 'one',
-            ],
-        ],
+        'phpdoc_line_span' => true,
+        'phpdoc_var_without_name' => false,
         'method_argument_space' => [
             'on_multiline' => 'ensure_fully_multiline',
             'keep_multiple_spaces_after_comma' => true,
         ],
-        'single_trait_insert_per_statement' => true,
     ])
     ->setFinder($finder);

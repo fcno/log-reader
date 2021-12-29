1f152bff5ad8de2515638fe2eb2bd75ee9da5cec
fcno <fabiocassiano@jfes.jus.br>
2021-12-29 02:12:53 -0300

    Implementar os testes para as novas exceptions

--- tests/Feature/RecordReader/ExceptionTest.php
+++ tests/Feature/RecordReader/ExceptionTest.php
@@ -7,10 +7,10 @@
  */
 
 use Fcno\LogReader\Exceptions\FileNotFoundException;
+use Fcno\LogReader\Exceptions\FileSystemNotDefinedException;
 use Fcno\LogReader\Exceptions\InvalidPaginationException;
 use Fcno\LogReader\Exceptions\NotDailyLogException;
 use Fcno\LogReader\Facades\RecordReader;
-use Fcno\LogReader\Tests\Stubs\LogGenerator;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
@@ -27,45 +27,47 @@ beforeEach(function () {
                             ->__toString();
 });
 
-test('lança exceção ao tentar ler registro de arquivo de log inexistente', function () {
+test('lança exceção ao acionar métodos get, infoAbout e paginate sem previamente definir o File System', function () {
     expect(
-        fn () => RecordReader::from($this->fs_name)
-                                ->infoAbout('laravel-2500-12-30.log')
-    )->toThrow(FileNotFoundException::class);
+        fn () => RecordReader::get($this->file_name)
+    )->toThrow(FileSystemNotDefinedException::class);
+
+    expect(
+        fn () => RecordReader::infoAbout($this->file_name)
+    )->toThrow(FileSystemNotDefinedException::class);
+
+    expect(
+        fn () => RecordReader::paginate(page: 2, per_page: 5)
+    )->toThrow(FileSystemNotDefinedException::class);
 });
 
-test('lança exceção ao tentar ler registro de arquivo de log com nome fora do padrão Laravel para logs diários', function () {
-    $new_name = 'laravel.log';
-
-    LogGenerator::on($this->fs_name)
-                ->create(null)
-                ->count(files: 1, records: 1);
-
-    $this->file_system->move(
-        from: $this->file_name,
-        to: $new_name
-    );
-
+test('lança exceção ao acionar método paginate com número da página ou com a quantidade de itens por página menor que 1', function () {
     expect(
         fn () => RecordReader::from($this->fs_name)
-                                ->infoAbout($new_name)
-    )->toThrow(NotDailyLogException::class);
-});
-
-test('lança exceção ao tentar paginar com número da página ou com a quantidade de itens por página menor que 1', function () {
-    LogGenerator::on($this->fs_name)
-                ->create(null)
-                ->count(files: 1, records: 1);
-
-    expect(
-        fn () => RecordReader::from($this->fs_name)
-                                ->infoAbout($this->file_name)
                                 ->paginate(page: -1, per_page: 1)
     )->toThrow(InvalidPaginationException::class);
 
     expect(
         fn () => RecordReader::from($this->fs_name)
-                                ->infoAbout($this->file_name)
                                 ->paginate(page: 1, per_page: -1)
     )->toThrow(InvalidPaginationException::class);
+});
+
+test('lança exceção ao acionar método infoAbout informando arquivo de log com nome fora do padrão Laravel para logs diários', function () {
+    // padrão correto é laravel-yyyy-mm-dd.log
+    $file_name = 'laravel.log';
+
+    expect(
+        fn () => RecordReader::from($this->fs_name)
+                                ->infoAbout($file_name)
+    )->toThrow(NotDailyLogException::class);
+});
+
+test('lança exceção ao acionar método infoAbout informando arquivo de log inexistente', function () {
+    $file_name = 'laravel-1500-01-30.log';
+
+    expect(
+        fn () => RecordReader::from($this->fs_name)
+                            ->infoAbout($file_name)
+    )->toThrow(FileNotFoundException::class);
 });

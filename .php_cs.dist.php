ef56417d1b61dd860cd8e7dab3a2bbec1cca64c6
fcno <fabiocassiano@jfes.jus.br>
2021-12-29 04:12:22 -0300

    Adicionar novas regras para o PHPCSFIXER
    - phpdoc_order
    - phpdoc_order_by_value
    - phpdoc_types_order
    - phpdoc_var_annotation_correct_order

--- .php_cs.dist.php
+++ .php_cs.dist.php
@@ -22,6 +22,14 @@ return (new PhpCsFixer\Config())
             'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
         ],
         'phpdoc_line_span' => true,
+        'phpdoc_order' => true,
+        'phpdoc_order_by_value' => [
+            'annotations' => ['author', 'covers', 'coversNothing', 'dataProvider', 'depends', 'group', 'internal', 'method', 'property', 'property-read', 'property-write', 'requires', 'throws', 'uses']
+        ],
+        'phpdoc_types_order' => [
+            'null_adjustment' => 'always_last', 'sort_algorithm' => 'alpha'
+        ],
+        'phpdoc_var_annotation_correct_order' => true,
         'phpdoc_var_without_name' => false,
         'method_argument_space' => [
             'on_multiline' => 'ensure_fully_multiline',
